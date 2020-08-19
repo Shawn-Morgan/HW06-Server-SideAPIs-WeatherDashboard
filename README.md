1b2c16c68f44afe5b5ac2b21abbd965cf300d892
shawnm7474 <shawnm74@gmail.com>
2020-08-18 23:45:35 -0500

    final submission - new uv

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HW06-Server-SideAPIs-WeatherDashboard
-weather dashboard
+## weather dashboard
+
+# api key = 1bbb3745f77de7afcb359aa19c1818f0
+https://home.openweathermap.org/api_keys
+
+# requirements --> 
+## search by city
+## current forecast
+## 5-day forecast
---
+++ weatherscript.js
@@ -0,0 +1,105 @@
+
+var searchButton = $(".searchBtn");
+
+
+//"weather dashboard api"
+var apiKey = "1bbb3745f77de7afcb359aa19c1818f0"
+
+
+
+//localstorage
+for (var i = 0; i < localStorage.length; i++) {
+
+    var city = localStorage.getItem(i);
+    
+    var cityName = $(".list-group").addClass("list-group-item");
+
+    cityName.append("<li>" + city + "</li>");
+}
+
+var keyCount = 0;
+//city search
+searchButton.click(function () {
+
+    var citySearch = $(".citySearch").val();
+
+    // Variable for current weather working 
+    var urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&Appid=" + apiKey + "&units=imperial";
+    // Variable for 5 day forecast working
+    var urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&Appid=" + apiKey + "&units=imperial";
+    //https://api.openweathermap.org/data/2.5/forecast?q=houston&Appid=1bbb3745f77de7afcb359aa19c1818f0&units=imperial
+
+
+    if (citySearch == "") {
+        console.log(citySearch);
+    } else {
+        $.ajax({
+            url: urlCurrent,
+            method: "GET"
+        }).then(function (response) {
+            // console.log(response.name);
+            console.log(response);
+            var cityName = $(".list-group").addClass("list-group-item");
+            cityName.append("<li>" + response.name + "</li>");
+            //storage
+            var local = localStorage.setItem(keyCount, response.name);
+            keyCount = keyCount + 1;
+
+            //append
+            var currentCard = $(".currentCard").append("<div>").addClass("card-body");
+            currentCard.empty();
+            var currentName = currentCard.append("<p>");
+            currentCard.append(currentName);
+
+            //date
+            var timeUTC = new Date(response.dt * 1000);
+            currentName.append(response.name + " " + timeUTC.toLocaleDateString("en-US"));
+            currentName.append(`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+            //temp 
+            var currentTemp = currentName.append("<p>");
+            
+            currentName.append(currentTemp);
+            currentTemp.append("<p>" + "Temperature: " + response.main.temp + "</p>");
+            //humidity
+            currentTemp.append("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
+            //wind 
+            currentTemp.append("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
+
+            //uv url
+
+            var urlUV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+
+            //uv
+
+            $.ajax({
+                url: urlUV,
+                method: "GET"
+            }).then(function (response) {
+
+            });
+
+        });
+
+        //5 day 
+        $.ajax({
+            url: urlFiveDay,
+            method: "GET"
+        }).then(function (response) {
+            // Array
+            var day = [0, 8, 16, 24, 32];
+            var fiveDayCard = $(".fiveDayCard").addClass("card-body");
+            var fiveDayDiv = $(".fiveDayOne").addClass("card-text");
+            fiveDayDiv.empty();
+            
+            day.forEach(function (i) {
+                var FiveDayTimeUTC1 = new Date(response.list[i].dt * 1000);
+                FiveDayTimeUTC1 = FiveDayTimeUTC1.toLocaleDateString("en-US");
+
+                fiveDayDiv.append("<div class=fiveDayColor>" + "<p>" + FiveDayTimeUTC1 + "</p>" + `<img src="https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png">` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>" + "</div>");
+
+
+            })
+
+        });
+    }
+});
